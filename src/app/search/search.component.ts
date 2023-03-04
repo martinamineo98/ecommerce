6dc4f9e23d6ad41c16a3836e0b20b97ba461f9b5
martinamineo98 <80021258+martinamineo98@users.noreply.github.com>
2023-03-04 21:35:28 +0100

    Fixed Search Function: it works!

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -13,6 +13,8 @@ import { AppComponent } from '../app.component';
 export class SearchComponent {
 
   products: any[] = []
+  categories: any[] = []
+  brands: any[] = []
 
   form = new FormGroup({
     cat: new FormControl(''),
@@ -26,6 +28,8 @@ export class SearchComponent {
 
   ngOnInit() {
     this.dataService.productsObservable.subscribe((products) => this.products = products)
+    this.dataService.categoriesObservable.subscribe((categories) => this.categories = categories)
+    this.dataService.brandsObservable.subscribe((brands) => this.brands = brands)
   }
 
   search(e: any) {
@@ -39,19 +43,52 @@ export class SearchComponent {
   }
   
   itContains(cat: any, str: any) {
-    let foundProducts = []
+    let foundProducts: any[] = []
+
+    function pushElement(element: any, statement: boolean) {
+      statement ? foundProducts.push(element) : null
+    }
 
     for (let product of this.products) {
       for (let [k, value] of Object.entries(product)) {
+
+        // fix unknown type error for value
+
+        let v: any = value
+
+        // ... first I need to check if k and cat are the same
+
+        // why a switch here ?
+        // summary: I don't know how to do it better
+
+        // name in product.json is a string
+        // brand in product.json is a number
+        // categories in product.json is an array
+
+        // in the case of name, I need to check if the string contains 'str'
+        // in the case of brand, I need to transform that number into a string (from data.json) and check if it contains the searched str
+        // in the case of categories, I need to transform each number into the array into a string (from data.json) and check if one of these contains the searched str
+
         if (k === cat) {
 
-          // fix to unknown type error again
-          let v: any = value
-
-          if (v.toLowerCase().includes(str)) {
-            foundProducts.push(product)
+          switch (cat) {
+            case 'name':
+              pushElement(product, product[cat].includes(str))
+              break
+            
+            case 'brand':
+              pushElement(product, this.brands[product[cat]].name.includes(str))
+              break
+  
+            case 'categories':
+              for (let element of product[cat]) {
+                pushElement(product, this.categories[element].name.includes(str))
+              }
+              break
           }
+
         }
+
       }
     }
 
