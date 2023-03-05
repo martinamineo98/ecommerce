f07c27608d0a0acee509eb46ec059b9172d468ec
martinamineo98 <80021258+martinamineo98@users.noreply.github.com>
2023-03-05 02:28:04 +0100

    Started working on UserComponent

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -5,6 +5,32 @@ import { Component } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
+
 export class UserComponent {
+
+  nav: any[] = [
+    {
+      str: 'account information',
+      content: 'a'
+    },
+    {
+      str: 'adress book',
+      content: 'b'
+    },
+    {
+      str: 'orders',
+      content: 'c'
+    },
+    {
+      str: 'settings',
+      content: 'd'
+    }
+  ]
+  
+  content: any = this.nav[0].content
+
+  changeContent(content: any) {
+    this.content = content
+  }
 
 }
